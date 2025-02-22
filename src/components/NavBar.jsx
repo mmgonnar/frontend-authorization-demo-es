bec414b8095658f091b3aeeb0e1d13e813c034fb
mmgonnar <gn.mariela@gmail.com>
2025-02-21 21:37:39 -0600

    Finished loggin out

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { removeToken } from "../utils/token";
 import Logo from "./Logo";
 import "./styles/NavBar.css";
 
-function NavBar() {
+function NavBar({ setIsLoggedIn }) {
+  const navigate = useNavigate();
+
+  function singOut() {
+    removeToken();
+    navigate("/login");
+    setIsLoggedIn(false);
+  }
   return (
     <div className="navbar">
       <div className="navbar__logo">
@@ -20,7 +28,9 @@ function NavBar() {
           </NavLink>
         </li>
         <li>
-          <button className="navbar__link navbar__button">Cerrar sesión</button>
+          <button className="navbar__link navbar__button" onClick={singOut}>
+            Cerrar sesión
+          </button>
         </li>
       </ul>
     </div>
